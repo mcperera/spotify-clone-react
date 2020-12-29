dc9140e01edb4a96f5986cfb64bd3fa4d258a810
Madushan Perera <madushanperera3817@gmail.com>
2020-12-29 16:00:06 +0530

    start footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -3,7 +3,13 @@ import React from "react";
 import "./Footer.css";
 
 function Footer() {
-  return <div className="footer__container">Footer</div>;
+  return (
+    <div className="footer__container">
+      <div className="footer_left">Left</div>
+      <div className="footer_center">Center</div>
+      <div className="footer_right">Right</div>
+    </div>
+  );
 }
 
 export default Footer;
