b04536123aeea20ea9140f6b86fa54a261303caf
Madushan Perera <madushanperera3817@gmail.com>
2020-12-31 00:43:35 +0530

    setup netlify

--- src/spotify.js
+++ src/spotify.js
@@ -1,7 +1,11 @@
 export const authEndpoint = "https://accounts.spotify.com/authorize";
 
-const redirectUri = "http://localhost:3000/";
-const clientId = "133100dd231f4e8596d26af6ab8c3979";
+// const redirectUri = "http://localhost:3000/";
+const redirectUri = "https://spotify-clone-react-mp.netlify.app";
+
+// const clientId = "133100dd231f4e8596d26af6ab8c3979";
+const clientId = "b6e45a0f200e4965860428dc1454e04e";
+
 const scopes = [
   "user-read-currently-playing",
   "user-read-recently-played",
